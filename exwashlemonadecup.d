c456c952abd49de9bb70146811cbb2d1d8eda4b7
ovidiucs <ovidiu.ctin@gmail.com>
2015-08-30 16:44:14 +0300

    problem

---
+++ exwashlemonadecup.d
@@ -0,0 +1,13 @@
+import std.stdio;
+
+void main()
+{
+   bool existsLemonade = true;
+
+   if (existsLemonade) {
+      writeln("Drinking lemonade");
+      writeln("Washing the cup");
+   } else
+      writeln("Eating pie");
+      writeln("Washing the plate");
+}
