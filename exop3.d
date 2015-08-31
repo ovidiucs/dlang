cc35e6d4eed07589832b6aa7d717cf45cb42517c
ovidiucs <ovidiu.ctin@gmail.com>
2015-08-31 20:17:52 +0300

    exercie 3

---
+++ exop3.d
@@ -0,0 +1,28 @@
+import std.stdio;
+
+void main()
+{
+   while (true) {
+      int num_1;
+      int num_2;
+      int opt;
+
+      write("Enter two numbers: ");
+      readf(" %s %s", &num_1, &num_2);
+
+      write("1: add, 2: subtract, 3: division, 4: multiplication. Any other
+            number - quit ");
+      readf(" %s", &opt);
+       if (opt == 1) {
+         writeln(num_1+num_2);
+      } else if (opt == 2) {
+         writeln(num_1-num_2); 
+      } else if (opt == 3) {
+         writeln(num_1 / num_2);
+      } else if (opt == 4) {
+         writeln(num_1 * num_2);
+      } 
+      break;
+
+   }
+}
