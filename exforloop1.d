d08bcf89511322f8acb79b4f45b8328c5d8da30f
ovidiucs <ovidiu.ctin@gmail.com>
2015-09-06 23:26:27 +0300

    exforloop1.d

--- exforloop1.d
+++ exforloop1.d
@@ -2,11 +2,9 @@ import std.stdio;
 
 void main()
 {
-   int[9][9] table;
    for (int i = 0; i < 9; ++i){
          for (int j=0; j<9; ++j){
-            table[0] = i;
-            table[1] = j;
+            writeln(i,j);
          }
    }
    writeln(table);
