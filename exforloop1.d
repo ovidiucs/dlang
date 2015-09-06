79fba450f1fc2a46c5c51117b7de7b20844276ad
ovidiucs <ovidiu.ctin@gmail.com>
2015-09-06 23:36:20 +0300

    exforloop1.d

--- exforloop1.d
+++ exforloop1.d
@@ -4,8 +4,8 @@ void main()
 {
    for (int i = 0; i < 9; ++i){
          for (int j=0; j<9; ++j){
-            write(j);
-            write(",",i);
+            write(j,",");
+            write(i);
          }
          writeln();
    }
