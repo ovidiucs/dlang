1c2ced93d5fefa4869463eb09bc53feb7095fcc3
ovidiucs <ovidiu.ctin@gmail.com>
2015-09-06 23:20:31 +0300

    exforloop1.d

--- exforloop1.d
+++ exforloop1.d
@@ -5,7 +5,7 @@ void main()
    int[9][9] table;
    for (int i = 0; i < 9; ++i){
          for (int j=0; j<9; ++j){
-            table[i][j] = i,j;
+            table[i][j] = [i,j];
          }
    }
    writeln(table);
