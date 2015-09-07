c03e0a7b427e938a417f6cd64238d4c09cb9b2ec
ovidiucs <ovidiu.ctin@gmail.com>
2015-09-07 13:03:55 +0300

    externary1.d

--- externary1.d
+++ externary1.d
@@ -6,6 +6,6 @@ void main()
    int value;
    write("Enter ammount negative or positive: ");
    readf("%s", &value);
-   writeln((value < 0) ? "$",to!string(value)," lost" : "$",
+   writeln((value > 0) ? "$",to!string(value)," lost" : "$",
          to!string(value)," gained");
 }
