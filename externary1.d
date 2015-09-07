bb29ff871518208de8949e82d6acf89d3acc3f60
ovidiucs <ovidiu.ctin@gmail.com>
2015-09-07 13:08:21 +0300

    externary1.d

--- externary1.d
+++ externary1.d
@@ -5,7 +5,7 @@ import std.string;
 void main()
 {
    write("Enter ammount negative or positive: ");
-   int value = chomp(readln());
+   int value = readln();
    writeln((value < 0) ? "$",to!string(value)," lost" : "$",
          to!string(value)," gained");
 }
